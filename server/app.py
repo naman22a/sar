f4082c46b208849034139a12d5ad0f1706783939
Naman Arora <namanarora1022@gmail.com>
2025-02-10 20:50:57 +0530

    refactor: app.py

--- server/app.py
+++ server/app.py
@@ -19,31 +19,31 @@ app.config['OUTPUT_FOLDER'] = OUTPUT_FOLDER
 
 @app.route('/colorize', methods=['POST'])
 def colorize():
-    # Validation
+    # validation
     if 'image' not in request.files:
         return jsonify({"error": "No file part"}), 400
     image = request.files['image']
     if image.filename == '':
         return jsonify({"error": "No selected file"}), 400
 
-    # Save original image
+    # save image to disk
     image_path = os.path.join(app.config["UPLOAD_FOLDER"], str(uuid.uuid4()) + '-' + image.filename)
     image.save(image_path)
 
-    # Preprocessing: Read and resize the image
+    # preprocessing the image
     img = cv2.imread(image_path).astype('float32')
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Ensure RGB format
-    img = cv2.resize(img, (SIZE, SIZE))  # Resize to model's expected size
-    img = img / 255.0  # Normalize pixel values
-    img = np.expand_dims(img, axis=0)  # Add batch dimension
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.resize(img, (SIZE, SIZE))
+    img = img / 255.0
+    img = np.expand_dims(img, axis=0)
 
-    # Prediction
+    # prediction
     prediction = model.predict(img)
 
-    # Post-processing: Convert back to an image format
+    # post processing the image
     output_img = (prediction[0] * 255).astype("uint8")
 
-    # Save processed image
+    # save processed image
     output_filename = os.path.join(app.config["OUTPUT_FOLDER"], "colorized_" + image.filename)
     cv2.imwrite(output_filename, cv2.cvtColor(output_img, cv2.COLOR_RGB2BGR))
 
