1b8b2eb7acec58c0ddded27a3ddef664482aa8db
Naman Arora <namanarora1022@gmail.com>
2025-02-11 15:21:02 +0530

    feat: setup client

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface Props {}
+
+const App: React.FC<Props> = () => {
+    return (
+        <div>
+            <h1>SAR</h1>
+        </div>
+    );
+};
+
+export default App;
